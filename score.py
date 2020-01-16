830f9979469c72b5a5226c4a0669d25f7eb7ed5c
Khanh Brandy <khanh.brandy@gmail.com>
2020-01-16 23:21:37 +0700

    Add files via upload

---
+++ score.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+def convert_score(self, probs):
+    pdo = 20
+    odd = 50
+    odds = (1 - probs) / probs
+    factor = pdo / np.log(2)
+    offset = 800 - factor * np.log(odd)
+    score = [offset + factor * np.log(i) for i in odds]
+    return score
+
+if __name__=='__main__':
+    score = convert_score(probs)
+    print('Score of the given user is {}'.format(score))
